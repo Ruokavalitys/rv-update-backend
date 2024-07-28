cb9237662b5e360a2faca832724a44bff57e6aa2
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2024-07-28 13:55:44 +0300

    Revert "Remove default margin route as it is replaced with preferences"
    This reverts commit fb5667f8e59272d1e8f7970631515aba781fdf4e.

---
+++ src/routes/admin/default_margin.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { getPreference, preferences, setPreference } from '../../db/preferences.js';
+import authMiddleware from '../authMiddleware.js';
+
+const { GLOBAL_DEFAULT_MARGIN } = preferences;
+
+const router = express.Router();
+
+router.use(authMiddleware('ADMIN', process.env.JWT_ADMIN_SECRET));
+
+router.get('/', async (_req, res) => {
+	const margin = await getPreference(GLOBAL_DEFAULT_MARGIN);
+	res.status(200).json({ margin });
+});
+
+router.patch('/', async (req, res) => {
+	await setPreference(GLOBAL_DEFAULT_MARGIN, req.body.margin);
+	res.status(200).send();
+});
+
+export default router;
